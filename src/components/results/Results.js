193e12c1321b82f87d4286d57658205610d27fe0
daisyy125 <daisy125@gmail.com>
2020-01-30 21:32:10 -0500

    added synonym component

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,33 +1,35 @@
 import React from 'react';
+// import Synonym from '../synonym/Synonym';
 
 function Results(props) {
-    const { results } = props;
+  const { results } = props;
 
-    if (results.meta && results.hwi && results.shortdef) {
-      return (
-        <div className='results'>
-          <h2>{results.meta.id}</h2>
-          <p>
-            <strong>Pronunciation:</strong>
-            <em> {results.hwi.prs && results.hwi.prs[0].mw}</em>
-          </p>
-          <p>
-            <strong>Definition:</strong>
-          </p>
-          <ol>
-            {results.shortdef &&
-              results.shortdef.map((result, index) => (
-                <li className='definition' key={index}>
-                  {result}
-                </li>
-              ))}
-          </ol>
-        </div>
-      );
-    } else {
-      return null;
-    }
-    
+  if (results.meta && results.hwi && results.shortdef) {
+    return (
+      <div className='results'>
+        <h2>{results.meta.id}</h2>
+        <p>
+          <strong>Pronunciation:</strong>
+          <em> {results.hwi.prs && results.hwi.prs[0].mw}</em>
+        </p>
+        <p>
+          <strong>Definition:</strong>
+        </p>
+        <ol>
+          {results.shortdef &&
+            results.shortdef.map((result, index) => (
+              <li className='definition' key={index}>
+                {result}
+              </li>
+            ))}
+        </ol>
+        {/* <Synonym /> */}
+      </div>
+    );
+  } else {
+    return null;
+  }
+
 
 }
 
---
+++ src/components/synonym/Synonym.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+
+// function Synonym(props) {
+//     const { syn } = props;
+
+//     return {
+//         console.log(syn)
+//     }
+//     // if (syn.meta) {
+//     //     return (
+//     //         <div className='results'>
+//     //             <em> {syn.syns}</em>
+//     //             <p>
+//     //                 <em> {syn.ants}</em>
+//     //             </p>
+//     //         </div>
+//     //     )
+//     // } else {
+//     //     return null;
+//     // }
+// }
+
+// export default Synonym;
