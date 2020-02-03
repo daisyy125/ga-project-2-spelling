9e0c5a487c9a6c70af189c0a8208f131ac8d0849
kchia <kchia87@gmail.com>
2020-02-03 17:04:30 -0500

    add feedback

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
-
+// Hou comment: take note of how I formatted the code for improved readability
 function Search(props) {
-    return (
-            <form onSubmit={props.handleSubmit} className='formParent' >
-                <input className='formItem' placeholder="Search Word" type="text" onChange={props.handleChange} value={props.searchWord}/>
-                <button type="submit" className='formItem'>BeeFind</button>
-            </form>
-    );
+  return (
+    <form onSubmit={props.handleSubmit} className="formParent">
+      <input
+        className="formItem"
+        placeholder="Search Word"
+        type="text"
+        onChange={props.handleChange}
+        value={props.searchWord}
+      />
+      <button type="submit" className="formItem">
+        BeeFind
+      </button>
+    </form>
+  );
 }
 
 export default Search;
--- src/components/search/Search.test.js
+++ src/components/search/Search.test.js
@@ -2,27 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Search from './Search';
 
+// Hou comment: remove any unnecessary comments/unused code from your files. Nice job with these tests!
 describe('Search Component', () => {
-    let component;
+  let component;
 
-    beforeEach(() => (
-        component = shallow(<Search />)
-    ))
-    
-    it('should have a form element with className formParent', () => {
-        // console.log('Hello World', component.find('.formParent').length)
-        expect(component.find('.formParent').length).toEqual(1);
-    });
+  beforeEach(() => (component = shallow(<Search />)));
 
-    it('should have a two elements with className formItem', () => {
-        // console.log('Hello World', component.find('.formItem').length);
-        expect(component.find('.formItem').length).toEqual(2);
-    })
+  it('should have a form element with className formParent', () => {
+    expect(component.find('.formParent').length).toEqual(1);
+  });
 
-    it('should have a button that submit search input', () => {
-        expect(component.contains('BeeFind')).toBe(true);
-    })
+  it('should have a two elements with className formItem', () => {
+    expect(component.find('.formItem').length).toEqual(2);
+  });
 
-})
+  it('should have a button that submit search input', () => {
+    expect(component.contains('BeeFind')).toBe(true);
+  });
+});
 
 export default Search;
